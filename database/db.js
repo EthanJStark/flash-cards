319fbe20c9732b712d4f5d32dbc3df6c5078c976
EthanJStark <ethanstark@gmail.com>
2016-09-13 15:09:27 -0700

    Created New Deck route + edit deck view. Fixes issues #22 #21
    * set up post deck view
    * WIP add
    * successful post-deck route, and edit view
    * changed req to request
    * addressed jrob comments

--- database/db.js
+++ database/db.js
@@ -4,8 +4,11 @@ const db = pgp( connection )
 
 const getAllDecks = 'SELECT * FROM decks'
 
+const newDeck = 'INSERT INTO decks (title) VALUES ($1)'
+
 const Decks = {
-  all: () => db.any( getAllDecks )
+  all: () => db.any( getAllDecks ),
+  new: title => db.one( newDeck, [title] )
 }
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,19 @@ const db = require('../database/db.js')
 const Decks = require('../database/db.js').Decks
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(request, response, next) {
   Decks.all()
-    .then( decks => res.render('decks/index', { decks } ) )
+    .then( decks => response.render('decks/index', { decks } ) )
+})
+
+router.get('/decks/new', ( request, response, next ) => {
+  response.render('decks/new')
+})
+
+router.post('/decks/new', ( request, response, next ) => {
+  const title = request.body.title
+  Decks.new( title )
+    .then( response.render('decks/new') )
 })
 
 module.exports = router
