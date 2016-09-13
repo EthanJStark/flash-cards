11d2971f66070a29aca656bb4ddd3ab7eba56171
EthanJStark <ethanstark@gmail.com>
2016-09-13 11:27:38 -0700

    Created all decks view and route (#24)
    * WIP create view for decks
    
    * blocked on db connection
    
    * Created all decks view and soute

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../database/db.js')
+const Decks = require('../database/db.js').Decks
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+  Decks.all()
+    .then( decks => res.render('decks/index', { decks } ) )
 })
 
 module.exports = router
