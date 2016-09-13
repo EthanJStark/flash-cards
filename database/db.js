b52249179adea75408411c1e3b4f1d4cf88dc51a
Ethan Stark <ethanstark@gmail.com>
2016-09-13 10:29:45 -0700

    blocked on db connection

--- database/db.js
+++ database/db.js
@@ -1,3 +1,13 @@
 const pgp = require( 'pg-promise' )()
 const connection = { database: 'flashcards' }
 const db = pgp( connection )
+
+const getAllDecks = 'SELECT * FROM decks'
+
+const Decks = {
+  all: () => db.any( getAllDecks )
+}
+
+module.exports = {
+  Decks
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../database/db.js')
+const Decks = require('../database/db.js').Decks
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('decks/index', { header: 'Express' })
+  const decks = Decks.all
+  console.log("decks.all", decks);
+  res.render('decks/index', { 
+    header: 'Decks',
+    decks: decks
+  })
 })
 
 module.exports = router
