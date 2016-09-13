6deb4b18d49cf16c760ef2a41ed43e7cd7376637
Ethan Stark <ethanstark@gmail.com>
2016-09-13 14:46:27 -0700

    changed req to request

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,20 @@ const db = require('../database/db.js')
 const Decks = require('../database/db.js').Decks
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(request, response, next) {
   Decks.all()
-    .then( decks => res.render('decks/index', { decks } ) )
+    .then( decks => response.render('decks/index', { decks } ) )
 })
 
-router.get('/decks/new', ( req, res, next ) => {
-  res.render('decks/new')
+router.get('/decks/new', ( request, response, next ) => {
+  response.render('decks/new')
 })
 
-router.post('/decks/new', ( req, res, next ) => {
-  const title = req.body.title
-  console.log('title', req.body.title);
+router.post('/decks/new', ( request, response, next ) => {
+  const title = request.body.title
+  console.log('title', request.body.title);
   Decks.new( title )
-    .then( res.render('decks/new') )
+    .then( response.render('decks/new') )
 })
 
 module.exports = router
