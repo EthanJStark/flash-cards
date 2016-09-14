aa8004845e5b367e02adfa5a0b6f7f6ba57a268a
Harman Singh <harmanlearns@gmail.com>
2016-09-14 15:25:04 -0700

    deleteCard query, and deleteCard function created

--- database/db.js
+++ database/db.js
@@ -8,13 +8,16 @@ const newDeck = 'INSERT INTO decks (title) VALUES ($1)'
 
 const newCard = 'INSERT INTO cards (front, back, deck_id) VALUES ($1, $2, $3)'
 
+const deleteCard = 'DELETE FROM cards WHERE id=$1'
+
 const Deck = {
   all: () => db.any( getAllDecks ),
   new: title => db.one( newDeck, [title] )
 }
 
 const Card = {
-  new: (front, back, id) => db.one( newCard, [front, back, id] )
+  new: (front, back, id) => db.one( newCard, [front, back, id] ),
+  delete: id => db.none( deleteCard, [id] )
 }
 
 module.exports = {
