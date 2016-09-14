ea5d52baf6d33120187302fc05f7e51bc7e7fdc2
Ethan Stark <ethanstark@gmail.com>
2016-09-14 13:48:32 -0700

    sql string, new card function

--- database/db.js
+++ database/db.js
@@ -6,13 +6,15 @@ const getAllDecks = 'SELECT * FROM decks'
 
 const newDeck = 'INSERT INTO decks (title) VALUES ($1)'
 
+const newCard = 'INSERT INTO cards (front, back, deck_id) VALUES ($1, $2, $3)'
+
 const Deck = {
   all: () => db.any( getAllDecks ),
   new: title => db.one( newDeck, [title] )
 }
 
 const Card = {
-  
+  new: (front, back, id) => db.one( newCard, [front, back, id] )
 }
 
 module.exports = {
