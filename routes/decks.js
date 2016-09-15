e15655d751714078faeb741615faad7ac4054954
EthanJStark <ethanstark@gmail.com>
2016-09-15 16:28:41 -0700

    Decks/new route working with empty object, new redirect routes
    * WIP making new deck page show blank page with dummy title
    
    * decks/new route working with empty object
    
    * removed comment
    
    * Correct redirects of new card + new deck
    
    * New Deck button redirects to correct page

--- routes/decks.js
+++ routes/decks.js
@@ -2,23 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Deck = require('../database/db.js').Deck
 
-//render edit / create deck page
 router.get('/new', ( request, response, next ) => {
-  response.render('decks/edit')
+  const cards = {}
+  response.render('decks/edit', { cards })
 })
 
-//edit page for a specific deck
 router.get('/edit/:id', ( request, response, next ) => {
   Deck.allCards( request.params.id )
-    .then( cards => response.render('decks/edit', { cards } ) )
-  //  console.log('cards', cards)
+    .then( cards => response.render('decks/edit', { cards } ))
 })
 
-//button for create a new deck. Need to insert dummy title
 router.post('/new', ( request, response, next ) => {
   const title = request.body.title
   Deck.create( title )
-    .then( response.render('decks/edit') )
+    .then( deck_id => response.redirect( `/decks/edit/${deck_id.id}` ))
 })
 
 module.exports = router
