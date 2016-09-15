407fb76b83efeb241e3d5f52128f6f94f2bf5eb8
Harman Singh <harmanlearns@gmail.com>
2016-09-15 10:07:56 -0700

    WIP making new deck page show blank page with dummy title

--- routes/decks.js
+++ routes/decks.js
@@ -4,21 +4,22 @@ const Deck = require('../database/db.js').Deck
 
 //render edit / create deck page
 router.get('/new', ( request, response, next ) => {
-  response.render('decks/edit')
+  const cards = {}
+
+  response.render('decks/edit', cards)
 })
 
 //edit page for a specific deck
 router.get('/edit/:id', ( request, response, next ) => {
   Deck.allCards( request.params.id )
     .then( cards => response.render('decks/edit', { cards } ) )
-  //  console.log('cards', cards)
 })
 
 //button for create a new deck. Need to insert dummy title
 router.post('/new', ( request, response, next ) => {
   const title = request.body.title
   Deck.create( title )
-    .then( response.render('decks/edit') )
+    .then( x => response.render('decks/edit') )
 })
 
 module.exports = router
