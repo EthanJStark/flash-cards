e79d545113955f2023a5b6b2e90ec2566ff090b8
Harman Singh <harmanlearns@gmail.com>
2016-09-15 09:21:16 -0700

    Refactored list-cards commments based on jrob feedback

--- routes/cards.js
+++ routes/cards.js
@@ -3,16 +3,19 @@ const router = express.Router()
 const Card = require('../database/db.js').Card
 
 //Render edit / create card page
-router.get('/cards/new', ( request, response, next ) => {
+router.get('/new', ( request, response, next ) => {
   response.render('cards/edit')
 })
 
 //button for saving a new card
-router.post('/cards/new', ( request, response, next ) => {
+router.post('/new', ( request, response, next ) => {
   const front = request.body.front
   const back = request.body.back
-  Card.new( front, back, 1 ) //currently, deck_id is hardcoded
-    .then( response.render('decks/edit') ) //should go back to specific deck
+
+  const deck_id = 1
+
+  Card.create( front, back, deck_id )
+    .then( deck => response.render( 'decks/edit', deck ) ) //should go back to specific deck
 })
 
 module.exports = router
