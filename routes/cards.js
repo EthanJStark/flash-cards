2ffd39577a194098cbbfa8c869b04e6bcad30c5f
Harman Singh <harmansingh@me.com>
2016-09-16 16:09:53 -0700

    Delete deck button works, deletes each deck, shows on index page

--- routes/cards.js
+++ routes/cards.js
@@ -8,14 +8,15 @@ router.get('/new', ( request, response, next ) => {
 })
 
 //button for saving a new card
+//deck/edit/:deckid/card/create
 router.post('/new', ( request, response, next ) => {
   const front = request.body.front
   const back = request.body.back
-
   const deck_id = 1
+  // console.log('deckid', deck_id)
 
   Card.create( front, back, deck_id )
-    .then( response.redirect( `/decks/edit/${deck_id}` ) ) //should go back to specific deck
+    .then( x => response.redirect( `/decks/edit/${deck_id.id}` ) ) //should go back to specific deck
 })
 
 module.exports = router
